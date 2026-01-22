ebe7b226873aaac775c06a05f07f0f757b5b2297
nilsw <nils.winterst001@gmail.com>
2026-01-22 13:06:52 +0100

    ccc

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -22,14 +22,21 @@ public class PrimeCheckTest {
   // --------------------------------------------------------------- //
   @Test
   public void testPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    int[] primes = {3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53};
+
+    for (int p : primes) {
+      assertEquals("Test prime " + p, true, PrimeCheck.isPrime(p));
+    }
+
   }
 
   // --------------------------------------------------------------- //
   @Test
   public void testNonPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    int[] primes = {4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26,27, 30, 32, 33, 34, 35, 36, 38, 40, 42, 44, 45, 48, 50, };
+        
+    for (int p : primes) {
+      assertEquals("Test nicht-prime " + p, false, PrimeCheck.isPrime(p));
+    }
   }
 }
